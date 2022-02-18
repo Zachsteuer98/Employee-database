b13fd79339fd2d6a4d9816f3fbfba1d554d877de
Zachsteuer98 <Zach.Steuer98@gmail.com>
2022-02-18 16:59:05 -0500

    hooked up mysql to node.js and launched the express.js server. tested a search query for the departments

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2')
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -6,6 +7,25 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+require('dotenv').config();
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: process.env.DB_USER,
+      // Your MySQL password
+      password: process.env.DB_PW,
+      database: process.env.DB_NAME
+    },
+    console.log('Connected to the employee database.')
+  );
+
+db.query('SELECT * FROM department', (err, rows) => {
+    console.log(rows);
+})
 
 app.get('/', (req, res) => {
     res.json({
