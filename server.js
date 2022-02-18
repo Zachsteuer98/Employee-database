01829af9adaace4364d37c104e996739a28ef5b1
Zachsteuer98 <Zach.Steuer98@gmail.com>
2022-02-18 17:20:24 -0500

    removed express because it is not required for the application to run. Insalled Inquirer and cTable. first db.query ran in the terminal with npm start

--- server.js
+++ server.js
@@ -1,12 +1,6 @@
 const mysql = require('mysql2')
-const express = require('express');
-
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+const inqurier = require('inquirer')
+const cTable = require('console.table')
 
 require('dotenv').config();
 
@@ -27,17 +21,3 @@ db.query('SELECT * FROM department', (err, rows) => {
     console.log(rows);
 })
 
-app.get('/', (req, res) => {
-    res.json({
-      message: 'Hello World'
-    });
-  });
-
-// Default response for any other request (Not Found)
-app.use((req, res) => {
-    res.status(404).end();
-  });
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
