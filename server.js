840f289b00dae6a1d1c5a9fdedb4d563c156d2b8
Zachsteuer98 <Zach.Steuer98@gmail.com>
2022-02-19 22:33:40 -0500

    Created add department function and am currently debugging add Employee function... error (val.type === 'seperator'). Will Work through this issue during my tutor session tommorow

--- server.js
+++ server.js
@@ -66,6 +66,21 @@ function runEmployeeDB() {
               viewAllEmployees();
               break;
 
+            //Add Department
+            case "Add Department":
+              addDepartment();
+              break;
+
+            //Add Role
+            case "Add Role":
+              addRole();
+              break;
+
+            //Add Employee
+            case "Add Employee":
+              addEmployee();
+              break;
+
             //EXIT
             case "Exit":
                 console.log ("===============================================");
@@ -93,7 +108,7 @@ function viewAllDepartments() {
   })
 }
 
-//View ALL Roles
+//View ALL Roles (need to connect departments to each role using primary and foreign keys)
 function viewAllRoles() {
 
   db.query("SELECT role.id AS id, role.title AS title, role.salary AS salary FROM role",
@@ -107,6 +122,7 @@ function viewAllRoles() {
   })
 }
 
+//View All Employees (need to connect Roles and Manager ID using primary and foreign keys)
 function viewAllEmployees() {
 
   db.query("SELECT employee.id AS id, employee.first_name AS first_name, employee.last_name AS last_name, employee.role_id AS role_id, employee.manager_id AS manager_id FROM employee",
@@ -120,5 +136,143 @@ function viewAllEmployees() {
   })
 }
 
+// ADD DEPARTMENT
+function addDepartment() { 
 
+  inquirer.prompt([
+      {
+        name: "name",
+        type: "input",
+        message: "What Department would you like to add?"
+      }
+
+  ])
+    .then(function(choices) {
+        db.query("INSERT INTO department SET ? ",
+            {
+              name: choices.name,
+            },
+            function(err, res) {
+                if (err) throw err
+                console.table(res);
+                runEmployeeDB();
+          }
+      )
+  })
+}
+
+// Select a Department Function
+var deptArr = [];
+function selectDepartment() {
+  db.query("SELECT * FROM department", function(err, res) {
+    if (err) throw err
+    for (var i = 0; i < res.length; i++) {
+      deptArr.push(res[i].name);
+    }
+})
+return deptArr;
+}
+
+// ADD ROLE
+function addRole() { 
+
+    inquirer.prompt([
+        {
+          name: "title",
+          type: "input",
+          message: "What is the name of the new role?"
+        },
+        {
+          name: "salary",
+          type: "input",
+          message: "What is the salary for the position?"
+        } ,
+        {
+          name: "department",
+          type: "rawlist",
+          message: "What department does this role fall under?",
+          choices: selectDepartment()
+        }
+    ]).then(function(choices) {
+        db.query(
+            "INSERT INTO role SET ?",
+            {
+              title: choices.title,
+              salary: choices.salary,
+              department_id: choices.department_id
+            },
+            function(err) {
+                if (err) throw err
+                console.table(choices);
+                runEmployeeDB();
+            }
+        )     
+    });
+  }
+
+  let roleArr = [];                                            
+  function selectRole() {
+    db.query("SELECT * FROM role", function(err, res) {
+      if (err) throw err
+      for (var i = 0; i < res.length; i++) {
+        roleArr.push(res[i].title);
+      }
+    })
+    return roleArr;
+  }
+
+// Select a Manager Function
+let managersArr = [];
+function selectManager() {
+  db.query("SELECT first_name, last_name FROM employee", function(err, res) {
+    if (err) throw err
+    for (var i = 0; i < res.length; i++) {
+      managersArr.push(res[i].firstName);
+    }
+  })
+  return managersArr;
+}
+
+// ADD New Employee error (val.type === 'seperator')
+function addEmployee() { 
+  inquirer.prompt([
+      {
+        name: "first_name",
+        type: "input",
+        message: "First Name: "
+      },
+      {
+        name: "last_name",
+        type: "input",
+        message: "Last Name: "
+      },
+      {
+        name: "role",
+        type: "list",
+        message: "What is the new employee's title? ",
+        choices: selectRole()
+      },
+      {
+          name: "manager_id",
+          type: "rawlist",
+          message: "Who is managing the new employee? ",
+          choices: selectManager()
+      }
+
+  ]).then(function (choices) {
+    db.query("INSERT INTO employee SET ?", 
+    {
+        first_name: choices.first_name,
+        last_name: choices.last_name,
+        role_id: choices.role_id,
+        manager_id: choices.manager_id
+        
+    }, 
+    function(err){
+        if (err) throw err
+        console.table(choices)
+        runEmployeeDB()
+    })
+  })
+}
 
